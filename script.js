c8589ace0d39ee06d84551d60c0a13b34fba2394
archistico <info@archistico.com>
2021-11-12 12:35:42 +0100

    aggiunto rapporto al select

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let dati = getDati();
 
 getDati().forEach((scala, index) => {
     let option = document.createElement("option");
-    option.text = "Dislivello: " + scala.dislivello.toFixed(1) + " cm | Alzata: " + scala.alzata.toFixed(1) + " cm | Pedata: " + scala.pedata.toFixed(1) + " cm | Numero alzate: " + scala.numeroAlzate + " | Sviluppo: " + scala.sviluppo.toFixed(2) + " cm";
+    option.text = "Dislivello: " + scala.dislivello.toFixed(1) + " cm | Alzata: " + scala.alzata.toFixed(1) + " cm | Pedata: " + scala.pedata.toFixed(1) + " cm | Numero alzate: " + scala.numeroAlzate + " | Sviluppo: " + scala.sviluppo.toFixed(1) + " cm | Rapporto: " + scala.rapporto;
     option.value = index
     stairSelect.add(option);
 });
