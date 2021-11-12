e527582beef33598243e946ffdde5ff9f4617808
archistico <info@archistico.com>
2021-11-12 16:41:36 +0100

    prima del disegno della scala

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 import { getDati } from './scala.js';
 
+// Dati impostati ora qua ma poi verranno messi nel form
+const avanzamentoPedata = 2;
+const larghezzaScala = 100;
+const spessoreRivestimento = 2;
+const spessoreBattiscopa = 10;
+
 let makeStairButton = document.getElementById('makeStairButton');
 let stairSelect = document.getElementById("stairSelect");
-
-makeStairButton.onclick = function (event) {
-    console.log(stairSelect.selectedIndex);
-}
 
 let dati = getDati();
 
@@ -15,3 +17,18 @@ getDati().forEach((scala, index) => {
     option.value = index
     stairSelect.add(option);
 });
+
+makeStairButton.onclick = function (event) {
+    let scala = dati[stairSelect.selectedIndex];
+    
+    let alzata = scala.alzata;
+    let pedata = scala.pedata;
+    let numeroAlzate = scala.numeroAlzate;
+    let rapporto = scala.rapporto;
+    let sviluppo = scala.sviluppo;
+    let dislivello = scala.dislivello;
+
+    // Disegna scala
+
+
+}
