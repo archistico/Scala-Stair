48e4c0838e2653a489bde3e98bdebf1f4e666c31
archistico <info@archistico.com>
2021-11-12 18:57:34 +0100

    add svg front

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 import { getDati } from './scala.js';
 
 // Dati impostati ora qua ma poi verranno messi nel form
-const avanzamentoPedata = 2;
-const larghezzaScala = 100;
-const spessoreRivestimento = 2;
-const spessoreBattiscopa = 10;
+let avanzamentoPedata = 2;
+let larghezzaScala = 100;
+let spessoreRivestimento = 2;
+let spessoreBattiscopa = 10;
 
 let makeStairButton = document.getElementById('makeStairButton');
 let stairSelect = document.getElementById("stairSelect");
@@ -28,7 +28,38 @@ makeStairButton.onclick = function (event) {
     let sviluppo = scala.sviluppo;
     let dislivello = scala.dislivello;
 
-    // Disegna scala
+    // TEMPORANEE PER DISEGNO SENZA SELEZIONE
+    if(stairSelect.selectedIndex == 0) {
+        alzata = 17.8;
+        pedata = 27.4;
+        numeroAlzate = 3;
+        rapporto = 63;
+        sviluppo = 54.8;
+        dislivello = 53.4;
+        avanzamentoPedata = 2;
+    }   
 
+
+    // Disegna scala
+    let drawTop = SVG('#drawing_top').size('100%', '100%');
+    //draw.size();
+    drawTop.viewbox(10, 10, 600, 200);
+
+    drawTop.clear();
+    
+    for(let c=0; c<numeroAlzate-1; c++) {
+        let line = drawTop.line(0, 0, 0, larghezzaScala).move(20+avanzamentoPedata+pedata*c,20)
+            line.stroke({ color: '#666666', width: 1, linecap: 'round', dasharray: '3,3' })
+        let rect = drawTop.rect(pedata, larghezzaScala).attr({ fill: '#dddddd', stroke: '#000','stroke-width': 1 }).move(20+pedata*c,20).back();
+        if(c==numeroAlzate-1) {
+            let rect = drawTop.rect(pedata, larghezzaScala).attr({ fill: '#dddddd', stroke: '#000','stroke-width': 1 }).move(20+pedata*c,20).back();
+        }
+    }
+
+    let drawFront = SVG('#drawing_front').size('100%', '100%');
+    //draw.size();
+    drawFront.viewbox(10, 10, 600, 200);
+
+    drawFront.clear();
 
 }
